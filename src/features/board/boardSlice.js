04fcbed4f45c34269b15bab11bdc6972c7b23e38
Jasvinder Singh <jvsinghk@gmail.com>
2021-05-25 14:16:24 +0530

    Create the Board Layout

--- src/features/board/boardSlice.js
+++ src/features/board/boardSlice.js
@@ -94,5 +94,5 @@ const initialState = [
   }
   
   // Add selector export statments below
-  
-  
+
+  export const selectBoard = state => state.board.map(card => ({id: card.id, contents: card.contents}));
