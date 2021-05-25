94be7136fa2fbab023ab3f5427255ef324d2c510
Jasvinder Singh <jvsinghk@gmail.com>
2021-05-25 14:04:46 +0530

    Provide redux store to application

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 
 import { store } from './app/store.js';
 // Add import statement below
+import { Provider } from 'react-redux';
 
 
 ReactDOM.render(
   // Implement Provider component with store below
-  
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
