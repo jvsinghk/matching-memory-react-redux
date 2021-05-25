c427ce138bc2f73deb12fed15841968d6efbd7f6
Jasvinder Singh <jvsinghk@gmail.com>
2021-05-25 14:01:16 +0530

    App Directories and Files Setup

--- src/App.js
+++ src/App.js
@@ -1,58 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import React from 'react';
+import { Score } from './features/score/Score.js';
+import { Board } from './features/board/Board.js';
+// Add import statements below
 
-function App() {
+
+
+const App = () => {
+  // Add dispatch variable below
+  
+
+  const startGameHandler = () => {
+    // Add action dispatch below
+    
+  };
+
+  const tryAgainHandler = () => {
+    // Add action dispatch below
+    
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Score />
+      <Board />
+      <footer className="footer">
+        <button onClick={startGameHandler} className="start-button">
+          Start Game
+        </button>
+        <button onClick={tryAgainHandler} className="try-new-pair-button">
+          Try New Pair
+        </button>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { combineReducers, createStore } from 'redux';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+import { boardReducer } from '../features/board/boardSlice.js';
+
+const rootReducer = combineReducers({
+  board: boardReducer,
 });
+
+export const store = createStore(rootReducer);
