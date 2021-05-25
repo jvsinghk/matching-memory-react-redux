30a61e7c10077c549bb4c45233a7dfae2222df47
Jasvinder Singh <jvsinghk@gmail.com>
2021-05-25 15:57:35 +0530

    Start the Game dispatch action setBoard

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import React from 'react';
 import { Score } from './features/score/Score.js';
 import { Board } from './features/board/Board.js';
 // Add import statements below
+import { useDispatch } from 'react-redux';
+import { setBoard } from './features/board/boardSlice';
 
 
 
 const App = () => {
   // Add dispatch variable below
-  
+  const dispatch = useDispatch();
 
   const startGameHandler = () => {
     // Add action dispatch below
-    
+    dispatch(setBoard());
   };
 
   const tryAgainHandler = () => {
